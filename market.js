812ba9ce2740d6111c50f230aca628f7930a8bac
HellcatFalcon <egor.savickiy2007@gmail.com>
2024-05-20 14:11:31 +0300

    123

--- market.js
+++ market.js
@@ -1,57 +1,48 @@
+// console.log('Перевірка підключеного файлу скриптів market.js')
 
-console.log('Перевірка підключеного файлу скриптів market.js')
+let itemsArray = [
+    {
+        firstName: "Віталій",
+        lastName: "Шатківський",
+        subject: "Інформатика",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/shatkivskyy-v-m/",
+    },
+    {
+    firstName: "Віталій",
+    lastName: "Шатківський",
+    subject: "Інформатика",
+    photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815-683x1024.jpg",
+    url: "https://lyceum.ztu.edu.ua/team/shatkivskyy-v-m/",
+    },
+]
 
 let itemsDiv = document.getElementById("items");
 
 if(itemsDiv) {
-    console.log(itemsDiv)
+    // console.log(itemsDiv)
 
-    console.log('Поле classList: ',itemsDiv.classList)
-    console.log('Поле id: ', itemsDiv.id)
-    console.log('Поле clientWidth', itemsDiv.clientWidth)
-    console.log('Поле innerHTML: ', itemsDiv.innerHTML)
+    // console.log('Поле classList: ',itemsDiv.classList)
+    // console.log('Поле id: ', itemsDiv.id)
+    // console.log('Поле clientWidth', itemsDiv.clientWidth)
+    // console.log('Поле innerHTML: ', itemsDiv.innerHTML)
 
-    itemsDiv.innerText = 'Програмно доданий текст'
-    itemsDiv.innerText += 'Другий програмно доданий текст'
+    // itemsDiv.innerText = 'Програмно доданий текст'
+    // itemsDiv.innerText += 'Другий програмно доданий текст'
 
-    itemsDiv.innerHTML = '<h1>Відформатований HTML код</h1>'
+    // itemsDiv.innerHTML = '<h1>Відформатований HTML код</h1>'
+    itemsArray.forEach((item, index) => {
+        itemsDiv.innerHTML +=
+        `
+        <div class = "item">
+            <h2>Вчитель №${index + 1} 3 ${itemsArray.length}</h2>
+            <p>${item.firstName} ${item.lastName}</p>
+            <p>Предмет: ${item.subject}</p>
+            <p><img src= "${item.photo}" class="item-image"></p>
+            <p><a href = "${item.url}" target="blank" class = "bonus price ">Профіль</a></p>
+        </div>
+        `
+    })
 } else {
     console.log('Блок товарів не знайдено')
 }
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
-    <title>Market</title>
-</head>
-<body>
-    <content>
-
-        <div class="title">Товари на які діє знижка</div>
-
-        <div id= "items" class="items"></div>
-        <div>
-            <p>Житомир</p>
-            <p>вул. Київська, 106</p>
-            <p>| +38 (067) 414-53-30*</p>
-        </div>
-        <div>*Знижка в розмірі сто гривень діє з Loren, 1psu
-            dolor
-            sit ant consectetur adipisicine elit. Ques officia
-            distinctio Itaque quisquan ratione fugit, incidunt sint,
-            corporis justo inventore facere. Voluptates,
-            temeti Veritatts er prouldent factls Susciplt eague
-            nica. Loren ipsun dolors Silt ence consectetur
-            Cipsanspit Comor gastepon foglat- Expedita.
-            accusantium Laborum soluta officIis, auten
-            คุนโรดุมลล enia erron sint expLicabo ad magnan a deserunt
-            qutexcepturt nemo.cciy</div>
-    </content>
-
-    <script src="market.js"></script>
-    
-</body>
-</html>
