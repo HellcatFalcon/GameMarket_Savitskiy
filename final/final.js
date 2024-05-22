c38d73450796e2cd8fe52ba92a78de0ae0c8007e
HellcatFalcon <egor.savickiy2007@gmail.com>
2024-05-22 14:47:40 +0300

    Final js

--- final/final.js
+++ final/final.js
@@ -1,15 +1,213 @@
-// console.log('Перевірка підключеного файлу скриптів market.js')
+
 
 let itemsArray = [
  {
-    title:
-    image:
-    mono:50
-    pb:30
-    prise_standart:1000
-    prise_discount:2000
-    prise_coupon:3000
-
+    email: "23b_lam@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Лещенко Настя",
+    projectName: "dude настюшкі",
+    mainRepository: "https://github.com/Leschenko1001/Phaser2ndGame1?authuser=0",
+    deployedSite: "https://leschenko1001.github.io/Phaser2ndGame1/?authuser=0",
+    driveFolder: "https://drive.google.com/drive/folders/1CqBXUS5Rk1u8nhEq3_l7LYMjDalYqPgW?usp=classroom_web&authuser=0",
+    googleForm: "https://forms.gle/aLmXJ7nEnjo6PGuZ8?authuser=0",
+    additionalRepository: "https://github.com/Leschenko1001/GameMarket_Leschenko",
+    additionalDeployedSite: "https://leschenko1001.github.io/GameMarket_Leschenko/?authuser=0",
+    marketHtml: "https://leschenko1001.github.io/GameMarket_Leschenko/market.html?authuser=0",
+    finalHtml: "https://leschenko1001.github.io/GameMarket_Leschenko/final/final.html?authuser=0",
+    authorPhoto: "unnamed.jpg",
+    gamePhoto: "URL_TO_GAME_PHOTO"
+  },
+  {
+    email: "23b_lsa@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Лобода Софія",
+    projectName: "MOKOSH",
+    mainRepository: "https://github.com/Lobodass/project.git",
+    deployedSite: "https://lobodass.github.io/project/",
+    driveFolder: "https://drive.google.com/drive/folders/1-0efrhem62Vc__8Ir6qIFuatZJ_FjV50?usp=classroom_web&authuser=0",
+    googleForm: "https://forms.gle/5DBw7bJ5v33uCqbt5",
+    additionalRepository: "https://github.com/Lobodass/siteinf.git",
+    additionalDeployedSite: "https://lobodass.github.io/siteinf/",
+    marketHtml: "https://lobodass.github.io/siteinf/marcet.html?authuser=0",
+    finalHtml: "https://lobodass.github.io/siteinf/final/final.html?authuser=0",
+    authorPhoto: "URL_TO_AUTHOR_PHOTO",
+    gamePhoto: "URL_TO_GAME_PHOTO"
+  },
+  {
+    email: "23b_nar@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Найбородін Артем",
+    projectName: "BestGame",
+    mainRepository: "https://github.com/Nayborodin/MyGame",
+    deployedSite: "https://nayborodin.github.io/MyGame/",
+    driveFolder: "https://drive.google.com/drive/folders/1G102lDz-nuYjqtOjxfLFwW2zB1R3BCgE",
+    googleForm: "https://forms.gle/vitg4Rk5EUm3MZe38",
+    additionalRepository: "https://github.com/Nayborodin/GameMarket_Nayborodin",
+    additionalDeployedSite: "https://nayborodin.github.io/GameMarket_Nayborodin/",
+    marketHtml: "https://nayborodin.github.io/GameMarket_Nayborodin/market.html",
+    finalHtml: "https://nayborodin.github.io/GameMarket_Nayborodin/final/final.html",
+    authorPhoto: "URL_TO_AUTHOR_PHOTO",
+    gamePhoto: "URL_TO_GAME_PHOTO"
+  },
+  {
+    email: "23b_nod@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Нікітіч Олександр",
+    projectName: "",
+    mainRepository: "https://github.com/SashaNikitich/Phaser2ndGame-2",
+    deployedSite: "https://sashanikitich.github.io/Phaser2ndGame-2/",
+    driveFolder: "https://docs.google.com/forms/d/e/1FAIpQLSdfx9mrL6L9s8P2Ratdn_TG0xqCTmyDBL3GfhUYM_27OnUmFA/viewform?usp=sharing, https://docs.google.com/document/d/14x-Cgw9X0pmnKD30qmBAKMQsE__y0-XQ/edit?usp=sharing&ouid=111241578456250236357&rtpof=true&sd=true",
+    googleForm: "https://forms.gle/k4FbZfE31MSNpNoe6?authuser=0",
+    additionalRepository: "https://github.com/SashaNikitich/Phaser2ndGame-2",
+    additionalDeployedSite: "https://sashanikitich.github.io/Phaser2ndGame-2/",
+    marketHtml: "",
+    finalHtml: "",
+    authorPhoto: "URL_TO_AUTHOR_PHOTO",
+    gamePhoto: "URL_TO_GAME_PHOTO"
+  },
+  {
+    email: "23b_pos@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Продоус Олександр",
+    projectName: "SuperGame",
+    mainRepository: "https://github.com/Oleksandrpr321/-Phaser2ndGame.git",
+    deployedSite: "https://oleksandrpr321.github.io/-Phaser2ndGame/",
+    driveFolder: "https://drive.google.com/drive/folders/15dRiiBT-A6CAsZa0mXBR7k9_2edGz6bP?usp=drive_link",
+    googleForm: "https://forms.gle/Y3SEVntm9Tr7znXu9",
+    additionalRepository: "https://github.com/Oleksandrpr321/-GameMarket_Prodous.git",
+    additionalDeployedSite: "https://oleksandrpr321.github.io/-GameMarket_Prodous/",
+    marketHtml: "https://oleksandrpr321.github.io/-GameMarket_Prodous/market.html",
+    finalHtml: "https://oleksandrpr321.github.io/-GameMarket_Prodous/final/final.html",
+    authorPhoto: "URL_TO_AUTHOR_PHOTO",
+    gamePhoto: "URL_TO_GAME_PHOTO"
+  },
+  {
+    email: "23b_pds@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Прокопчук Денис",
+    projectName: "Апокаліпсіс",
+    mainRepository: "https://github.com/Denis16543/Phaser2ndGame",
+    deployedSite: "https://denis16543.github.io/Phaser2ndGame/",
+    driveFolder: "https://drive.google.com/drive/folders/1KKIoePi4fkPjpWY4mAYEOL99wByIXX_t?usp=drive_link",
+    googleForm: "https://forms.gle/yn4n8Gsu67vAtx8S9",
+    additionalRepository: "https://github.com/Denis16543/GameMarket",
+    additionalDeployedSite: "https://denis16543.github.io/GameMarket/",
+    marketHtml: "https://denis16543.github.io/GameMarket_Prokopchuk/",
+    finalHtml: "https://denis16543.github.io/GameMarket_Prokopchuk/final/final.html",
+    authorPhoto: "URL_TO_AUTHOR_PHOTO",
+    gamePhoto: "URL_TO_GAME_PHOTO"
+  },
+  {
+    email: "23b_syei@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Савицкьий Єгор",
+    projectName: "Falcon",
+    mainRepository: "https://github.com/HellcatFalcon/game.git",
+    deployedSite: "https://github.com/HellcatFalcon/Faclon/settings/pages",
+    driveFolder: "https://drive.google.com/drive/folders/1yCCzefMmd5BGGz9UBrljEXQWQThK9ImN?usp=drive_link",
+    googleForm: "https://forms.gle/jAf9YW87aQqeoj4v6",
+    additionalRepository: "https://github.com/HellcatFalcon/GameMarket_Savitskiy.git",
+    additionalDeployedSite: "https://hellcatfalcon.github.io/GameMarket_Savitskiy/",
+    marketHtml: "http://127.0.0.1:5500/market.html",
+    finalHtml: "http://127.0.0.1:5500/final/final.html",
+    authorPhoto: "URL_TO_AUTHOR_PHOTO",
+    gamePhoto: "URL_TO_GAME_PHOTO"
+  },
+  {
+    email: "23b_stv@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Саух Тимофій",
+    projectName: "якісь колодязі",
+    mainRepository: "https://github.com/imnottimaw/Phaser2ndGame",
+    deployedSite: "https://imnottimaw.github.io/Phaser2ndGame/",
+    driveFolder: "https://drive.google.com/drive/folders/1YvPNGVqCD6BBzWr4azgHLxwar91rtT6H?usp=sharing",
+    googleForm: "https://forms.gle/a58ghZiZdVJfZVLg6",
+    additionalRepository: "https://github.com/imnottimaw/GameMarket_Saukh",
+    additionalDeployedSite: "https://imnottimaw.github.io/GameMarket_Saukh/",
+    marketHtml: "https://imnottimaw.github.io/GameMarket_Saukh/final/final.html",
+    finalHtml: "https://imnottimaw.github.io/GameMarket_Saukh/",
+    authorPhoto: "URL_TO_AUTHOR_PHOTO",
+    gamePhoto: "URL_TO_GAME_PHOTO"
+  },
+  {
+    email: "23b_som@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Скримінська Оксана",
+    projectName: "",
+    mainRepository: "https://github.com/skryminska/Phaser2ndGame",
+    deployedSite: "https://skryminska.github.io/Phaser2ndGame/",
+    driveFolder: "https://docs.google.com/document/d/19B0k9awxwUCrPf3vL68ekIFYvMzU3q-tIg8Q3hMjbHQ/edit?usp=classroom_web&authuser=0",
+    googleForm: "",
+    additionalRepository: "https://github.com/skryminska/GameMarket_Skryminska",
+    additionalDeployedSite: "https://skryminska.github.io/GameMarket_Skryminska/",
+    marketHtml: "",
+    finalHtml: "",
+    authorPhoto: "URL_TO_AUTHOR_PHOTO",
+    gamePhoto: "URL_TO_GAME_PHOTO"
+  },
+  {
+    email: "23b_sns@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Смолянова Ніка",
+    projectName: "Exiting way",
+    mainRepository: "https://github.com/Nikasms/-Phaser2ndGame.git",
+    deployedSite: "https://nikasms.github.io/-Phaser2ndGame/",
+    driveFolder: "https://drive.google.com/drive/folders/1oNBngdHeURy6zi-iwPPfUc8XsuIx5Ekk",
+    googleForm: "https://forms.gle/MU9K5RhTwm4SJAXeA",
+    additionalRepository: "https://github.com/Nikasms/GameMarket_Smolianova.git",
+    additionalDeployedSite: "https://nikasms.github.io/GameMarket_Smolianova/",
+    marketHtml: "https://nikasms.github.io/GameMarket_Smolianova/market.html",
+    finalHtml: "https://nikasms.github.io/GameMarket_Smolianova/final/final.html",
+    authorPhoto: "URL_TO_AUTHOR_PHOTO",
+    gamePhoto: "URL_TO_GAME_PHOTO"
+  },
+  {
+    email: "23b_tdp@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Терський Дмитро",
+    projectName: "",
+    mainRepository: "https://github.com/dmitro321w/GameMarket_-/tree/main",
+    deployedSite: "https://github.com/dmitro321w/GameMarket_-/tree/main",
+    driveFolder: "https://drive.google.com/drive/folders/1izA5dAz4VlfRCffwQCXKGeLpKX3qpDGA?usp=drive_link",
+    googleForm: "1",
+    additionalRepository: "https://github.com/dmitro321w/GameMarket_-/tree/main",
+    additionalDeployedSite: "https://dmitro321w.github.io/GameMarket_-/",
+    marketHtml: "",
+    finalHtml: "",
+    authorPhoto: "URL_TO_AUTHOR_PHOTO",
+    gamePhoto: "URL_TO_GAME_PHOTO"
+  },
+  {
+    email: "23b_shmv@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Шапіренко Максим",
+    projectName: "Roborun",
+    mainRepository: "https://github.com/Shapirenko/Roborun",
+    deployedSite: "https://shapirenko.github.io/Roborun/",
+    driveFolder: "https://drive.google.com/drive/folders/1j-uM3IKdJUH3IbqpNKxAvkDJvP_XrunA?usp=classroom_web&authuser=0",
+    googleForm: "https://forms.gle/Ty19Ttoo6CWEGdp78?authuser=0",
+    additionalRepository: "https://github.com/Shapirenko/GameMarket_Shapirenko?authuser=0",
+    additionalDeployedSite: "https://shapirenko.github.io/GameMarket_Shapirenko/?authuser=0",
+    marketHtml: "https://shapirenko.github.io/GameMarket_Shapirenko/market.html?hl=ru&authuser=0",
+    finalHtml: "https://shapirenko.github.io/GameMarket_Shapirenko/final/final.html?authuser=0",
+    authorPhoto: "URL_TO_AUTHOR_PHOTO",
+    gamePhoto: "URL_TO_GAME_PHOTO"
+  },
+  {
+    email: "23b_svyu@liceum.ztu.edu.ua",
+    class: "10-Б 2",
+    name: "Соколовська Вікторія",
+    projectName: "Космічне сміття",
+    mainRepository: "https://github.com/VikaSokol/Space-Trash-",
+    deployedSite: "https://vikasokol.github.io/Space-Trash-/",
+    driveFolder: "https://drive.google.com/drive/folders/10W9NbLTXlXtVCFWZfdoyVVfpVku7qJwA?usp=drive_link",
+    googleForm: "https://drive.google.com/drive/folders/10W9NbLTXlXtVCFWZfdoyVVfpVku7qJwA?usp=drive_link",
+    additionalRepository: "https://github.com/VikaSokol/GameMarket_Sokolovska",
+    additionalDeployedSite: "https://vikasokol.github.io/Space-Trash-/",
+    marketHtml: "https://vikasokol.github.io/market/market.html?authuser=0",
+    finalHtml: "",
+    authorPhoto: "URL_TO_AUTHOR_PHOTO",
+    gamePhoto: "URL_TO_GAME_PHOTO"
  }
     
 ]
@@ -17,25 +215,19 @@ let itemsArray = [
 let itemsDiv = document.getElementById("items");
 
 if(itemsDiv) {
-    // console.log(itemsDiv)
-
-    // console.log('Поле classList: ',itemsDiv.classList)
-    // console.log('Поле id: ', itemsDiv.id)
-    // console.log('Поле clientWidth', itemsDiv.clientWidth)
-    // console.log('Поле innerHTML: ', itemsDiv.innerHTML)
-
-    // itemsDiv.innerText = 'Програмно доданий текст'
-    // itemsDiv.innerText += 'Другий програмно доданий текст'
-
-    // itemsDiv.innerHTML = '<h1>Відформатований HTML код</h1>'
     itemsArray.forEach((item, index) => {
         itemsDiv.innerHTML +=
         ` <div class = "item">
         
-        <div class ="item-title"> $(item.title)</div>
+        <div class ="projectName">${item.projectName}</div>
         
         <!-- Блок товару(малюнок)--!>
-        <div class="item-image">$(item.image)</div>
+        <div class="gamePhoto">${item.gamePhoto}</div>
+
+        <div class="email">${item.email}</div>
+        <div class="name">${item.name}</div>
+        <div class="item-image">
+        <img src="img/${item.authorPhoto}" class = "item-image">
         `
     })
 } else {
